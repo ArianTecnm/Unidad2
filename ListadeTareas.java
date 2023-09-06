d195b2b5137f4c773cb9fac40a00bfa13fcf9b94
ArianTecnm <143622352+ArianTecnm@users.noreply.github.com>
2023-09-06 08:42:30 -0600

    Add files via upload

---
+++ ListadeTareas.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class ListadeTareas {
+    public static void main(String[]args) {
+ 
+    String Tarea1 ="Lavar La Ropa";
+    String Tarea2 ="LimpiarElCuarto";
+    String Tarea3 ="HacerEjercicosDeCalculo";
+    String Tarea4 ="Programar";
+    String Tarea5 ="RealizarPresentacionDeETICA";
+    System.out.println("Mis tareas son: " + Tarea1+Tarea2 +Tarea3+Tarea4+Tarea5);
+    }
+
+}
