2824624d07d0a378de5ecaa53bca07ee8033ea42
ArianTecnm <143622352+ArianTecnm@users.noreply.github.com>
2023-09-11 16:35:11 -0600

    Add files via upload

---
+++ AreaDeUnTriangulo.java
@@ -0,0 +1,24 @@
+/*
+
+ */
+package area.de.un.triangulo;
+
+/**
+ *
+ * @author Arian
+ */
+public class AreaDeUnTriangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int Base=10;
+        int Altura=5;
+        int Area= Base*Altura/2;
+        System.out.println("El area del Triangulo es: " +Area);
+        
+    }
+    
+}
